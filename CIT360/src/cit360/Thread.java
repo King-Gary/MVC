3a7a6353204c74c8fc4bd924b6cbe9e53f78495d
Gary <Gary@DESKTOP-N5T8JS1>
2019-03-04 17:49:25 -0500

    class bunch added.

---
+++ CIT360/src/cit360/Thread.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit360;
+
+/**
+ *
+ * @author Gary
+ */
+public class Thread {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Runnable task = () -> {
+    String threadName = Thread.currentThread().getName();
+    System.out.println("Hello " + threadName);
+    };
+
+        task.run();
+
+    Thread thread = new Thread(task);
+    thread.start();
+
+    System.out.println("Done!");
+    sleep();
+    executor();
+    }
+    
+private static void executor() {
+//Executor
+    {
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    executor.submit(() -> {
+    String threadName = Thread.currentThread().getName();
+    System.out.println("Hello ex" + threadName);
+    
+    //shuts it down
+    executor.shutdownNow();
+    
+});
+}
+    
+}
+    
+    
+
+    private static void sleep() {
+        Runnable runnable = () -> {
+    try {
+        String name = Thread.currentThread().getName();
+        System.out.println("Foo " + name);
+        Thread.sleep(1000);
+        System.out.println("Bar " + name);
+    }
+    catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+};  
+
+Thread thread = new Thread(runnable);
+thread.start();
+}
+}
